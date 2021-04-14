2ca9baf0b2f30efae1b28fc6ef3476c0d9e79e26
Danny Sollomoni <dsollomoni@gmail.com>
2021-04-14 18:41:05 -0400

    managed to make prompts work

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,28 @@
 // Assignment Code
+
+var lower ="abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var number = "0123456789";
+var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+function generatePassword(){
+  prompt("How long do you want your password to be? (8-128 charcters)")
+  var length = " "; 
+  if (length < 8 && length > 128) {
+  }
+  var r = confirm("Do you want lowercase letters?");
+  if (r == true) {
+    
+  }
+}
+
+//var lowerOp 
+//var upperOp
+//var numberOp
+//var specialOp
+
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
