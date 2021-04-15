e81b9d61235ea317532951356608ff6db0801da5
Danny Sollomoni <dsollomoni@gmail.com>
2021-04-14 20:13:32 -0400

    creating generator

--- Develop/script.js
+++ Develop/script.js
@@ -3,23 +3,58 @@
 var lower ="abcdefghijklmnopqrstuvwxyz";
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var number = "0123456789";
-var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var special = "!#$%&'()*+,-./:;<=>?@[^_`{|}~";
+
+var lowerOp;  
+var upperOp;
+var numberOp;
+var specialOp;
 
 function generatePassword(){
-  prompt("How long do you want your password to be? (8-128 charcters)")
-  var length = " "; 
-  if (length < 8 && length > 128) {
+  var length = " ";
+  while (length < 8 || length > 128 ) {
+    length = prompt("How long do you want your password to be? (8-128 charcters)"); 
+    if (length === null){
+      break;
+    }
   }
-  var r = confirm("Do you want lowercase letters?");
-  if (r == true) {
+  if (length){ 
+    if (confirm("Do you want lowercase letters?") == true) {
+      random += lower;
+    }
+
+    if(confirm("Do you want uppercase letters?") == true ) {
+      random += upper; 
+    }
+
+    if(confirm("Do you want numbers?") == true ) {
+      random += number; 
+    }
+    if(confirm("Do you want special characters?") == true){
+      random += special; 
+    }
+
+    //Checks if all are false
+    if (!lowerOp && !upperOp && !numberOp && !specialOp) {
+      alert("you must select one type.")
+    }
+  } 
+    
+  var random = '';
+  var randomPass = '';
+  for(var i = 0; i < length; i++) {
+    
+    randomPass += random[Math.floor(Math.random() * random.length)];
     
   }
+
+  
+  
+
+    
+  
 }
 
-//var lowerOp 
-//var upperOp
-//var numberOp
-//var specialOp
 
 var generateBtn = document.querySelector("#generate");
 
